ea4bdc5d5d39570f1a548e0c4ff2a9ed40142db8
Muhammad-Adhitya-Diandra-Nugraha <6182301042@student.unpar.ac.id>
2025-11-28 22:30:35 +0700

    Fitur Channel Individu

---
+++ Youtube/src/Controller.java
@@ -0,0 +1,26 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Controller {
+    private User user;
+
+    private Controller() {
+        publicList = new VideoList();
+    }
+
+    public static Controller getInstance() {
+        if (instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+    public boolean makeChannel(String nama, String deskripsi, int tipeChannel) throws SQLException {
+        channel = Channel.makeChannel(user.getIdPengguna(), nama, deskripsi, tipeChannel);
+        channel.import_ID_for_NewChannel(nama, deskripsi, tipeChannel);
+        channel.exportChannel();
+
+        return channel != null;
+    }
+}
