5f3e86a9396b7095fbdb6dc81e0e2653b878826d
KennethNathanael3 <kennethnathanael3105@gmail.com>
2025-11-28 22:32:43 +0700

    fitr ChannelGrup

---
+++ Youtube/src/MainApp.java
@@ -0,0 +1,91 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+public class MainApp {
+    public static Controller controller = Controller.getInstance();
+    public static ConnectDB konektor = ConnectDB.getInstance();
+    public static boolean logOut = false;
+    public static void main(String[] args) throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        int action;
+        boolean checkAction = false;
+
+        System.out.println("=== WELCOME TO YOUTUBE ===");
+        while (true) {
+            printCommand(new String[] { "Register", "Login", "Video Page", "Exit" });
+            System.out.print("Pick your Action: ");
+            action = sc.nextInt();
+            System.out.println();
+            switch (action) {
+                case 1:
+                    checkAction = registerPage(sc);
+                    break;
+                case 2:
+                    checkAction = loginPage(sc);
+                    break;
+                case 3:
+                    videoPage(sc);
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    return;
+                default:
+                    System.out.println("Command is not valid!");
+                    break;
+            }
+            if (checkAction) {
+                if (controller.importBrandchannel()) {
+                    if (controller.importChannel()) {
+                        homePageContentCreatorHaveChannel(sc);
+                    }
+                    else {
+                        homePageContentCreatorNoChannel(sc);
+                    }
+                }
+                else {
+                    if (controller.importChannel()) {
+                    homePageHaveChannel(sc);
+                    }
+                    else {
+                        homePageNoChannel(sc);
+                    }
+                }
+            }
+        }
+    }
+
+    // PAGE MEMBUAT CHANNEL - BARIS 183-203
+    public static boolean makeChannelPage(Scanner sc) throws SQLException {
+        System.out.println();
+        String nama, deskripsi;
+        int tipeChannel;
+
+        System.out.print("Channel Name : ");
+        sc.nextLine();
+        nama = sc.nextLine();
+
+        System.out.print("Channel Description : ");
+        deskripsi = sc.nextLine();
+
+        System.out.println("==== CHANNEL TYPE (Individual = 1 | Group = 2) ====");
+        System.out.println("1. Individual");
+        System.out.println("2. Group");
+        System.out.print("Channel type (1 / 2): ");
+        tipeChannel = sc.nextInt();
+
+        return controller.makeChannel(nama, deskripsi, tipeChannel);
+    }
+
+    public static void printCommand(String[] command) {
+        System.out.println("==================");
+        for (int i = 0; i < command.length; i++) {
+            System.out.printf("[%d] %s\n", i + 1, command[i]);
+        }
+        System.out.println("==================");
+    }
+
+    public void getCurrentUser() {
+        // return user;
+    }
+}
